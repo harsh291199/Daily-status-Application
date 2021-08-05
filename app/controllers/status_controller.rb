2f1984d0965ac214956277c0f1bfb49f51b36b81
harsh291199 <harshsuthar2911@gmail.com>
2021-08-05 15:15:45 +0530

    Add controller and basic action views

---
+++ app/controllers/status_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Status Controller
+class StatusController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+end
