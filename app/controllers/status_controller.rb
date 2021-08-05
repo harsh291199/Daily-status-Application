76cda09daaeedfe8a8b396657003429f8033b22f
harsh291199 <harshsuthar2911@gmail.com>
2021-08-05 18:43:02 +0530

    Add remaining actions in controller and partial in views

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -2,21 +2,50 @@
 
 # Status Controller
 class StatusController < ApplicationController
+  before_action :set_status, only: %i[show edit update destroy]
+
   def index
     @statuses = Status.all
   end
 
-  def show
-    @status = Status.find(params[:id])
+  def show; end
+
+  def new
+    @status = Status.new
   end
 
-  def new; end
+  def create
+    @status = Status.new(status_params)
 
-  def create; end
+    if @status.save
+      redirect_to @status
+    else
+      render 'new'
+    end
+  end
 
   def edit; end
 
-  def update; end
+  def update
+    if @status.update(status_params)
+      redirect_to @status
+    else
+      render 'edit'
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @status.destroy
+    redirect_to status_index_path
+  end
+
+  private
+
+  def set_status
+    @status = Status.find(params[:id])
+  end
+
+  def status_params
+    params.require(:status).permit(:date, :tasks, :pr_links, :hours)
+  end
 end
